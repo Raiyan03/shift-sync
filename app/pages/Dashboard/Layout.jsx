7b634dcf8aec9cfdb4873829294dcb256ddaa48e
Jayden <132015515+JaydenL-YVR@users.noreply.github.com>
2024-05-22 20:27:25 -0600

    Add files via upload

---
+++ app/pages/Dashboard/Layout.jsx
@@ -0,0 +1,83 @@
+
+"use client"
+import { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemText, Typography, Box, Button } from '@mui/material';
+// import MenuIcon from '@mui/icons-material/Menu';
+import { doSignOut } from '@/app/components/auth';
+
+const drawerWidth = 240;
+
+const Layout = ({ children }) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <List>
+        {['Dashboard', 'Reports', 'Settings'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Manager Dashboard
+          </Typography>
+
+          <Button onClick={doSignOut()}>Log Out</Button>
+
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        {drawer}
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
---
+++ app/pages/ManagerSide/page.js
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const ManagerSide = ({ children }) => {
+  return (
+    <>
+      <div>
+        <nav className="bg-white h-20 w-screen" >
+
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default ManagerSide;

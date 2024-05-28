8fa0ab70c1e05bb18683aa08e9f696146426e68b
Jayden <132015515+JaydenL-YVR@users.noreply.github.com>
2024-05-28 12:40:00 -0600

    Update sidepane.jsx
    Used link from next/link instead

--- app/pages/Manager/comps/sidepane.jsx
+++ app/pages/Manager/comps/sidepane.jsx
@@ -1,23 +1,30 @@
+// /app/Manager/comps/sidepane.js
 "use client";
+import React from "react";
+import Link from "next/link";
 
 const SidePane = () => {
   return (
-    <div className="flex flex-1">
-      <aside className="w-64 bg-gray-400 p-4 text-black">
-        <nav>
-          <ul>
-            <li className="mb-4">
-              <Link href="/pages/Employee/pages/Schedule">Schedule</Link>
-            </li>
-            <li className="mb-4">
-              <Link href="/pages/Employee/pages/Preference">Preferences</Link>
-            </li>
-            <li>
-              <Link href="/pages/Employee/pages/Info">Info</Link>
-            </li>
-          </ul>
-        </nav>
-      </aside>
+    <div className="w-64 bg-gray-400 p-4 text-black h-full">
+      <nav>
+        <ul>
+          <li className="mb-4">
+            <Link href="/pages/Employee/pages/Schedule">
+              <div className="block text-white hover:bg-gray-500 py-2 px-4 rounded">Schedule</div>
+            </Link>
+          </li>
+          <li className="mb-4">
+            <Link href="/pages/Employee/pages/Preference">
+              <div className="block text-white hover:bg-gray-500 py-2 px-4 rounded">Preferences</div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/pages/Employee/pages/Info">
+              <div className="block text-white hover:bg-gray-500 py-2 px-4 rounded">Info</div>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };

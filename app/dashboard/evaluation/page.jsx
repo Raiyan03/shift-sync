e4d04d38af38b4aa2073727f075718087d66409b
Dhillon-Saab <ad70138@gmail.com>
2024-06-27 10:53:23 -0600

    Updated Frontend

---
+++ app/dashboard/evaluation/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+import React from "react";
+import Evaluation from "./evaluation";
+
+export default function EvaluationPage() {
+  return (
+   
+    <div>
+      <Evaluation />
+    </div>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,13 @@
+export {auth as middleware} from "@/auth"
+// import { auth } from "@/auth"
+ 
+// export default auth((req) => {
+//   if (!req.auth && req.nextUrl.pathname !== "/login") {
+//     const newUrl = new URL("/login", req.nextUrl.origin)
+//     return Response.redirect(newUrl)
+//   }
+// })
+
+// export const config = {
+//     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+//   }

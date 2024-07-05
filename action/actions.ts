12cc01cbfbc2b57d6a929a72a165d0bb2c736bf2
Dhillon-Saab <ad70138@gmail.com>
2024-07-04 18:03:45 -0600

    Implemented the manager register page

--- action/actions.ts
+++ action/actions.ts
@@ -14,6 +14,7 @@ const loginUser = async (formData: FormData) =>{
     try{
        await signIn('credentials',{
         redirect: false,
+        redirectTo: '/',
         callbackUrl: '/',
         email: email,
         password: password,
@@ -37,7 +38,7 @@ const registerUser = async (formData: FormData)=>{
     
     const id = Math.floor(Math.random() * 100000);
 
-    const realId = `${teamName.slice(0,4).toLowerCase()}-${5}${id}`;
+    const realId = `${teamName.slice(0,6).toLowerCase()}-${5}${id}`;
 
     const hashedPassword = await saltAndHashPassword(password)
     
@@ -55,7 +56,9 @@ const registerUser = async (formData: FormData)=>{
             await setDoc(doc(db, `Organizations/`, realId),{
                 name: name,
                 email: email,
+                organization: teamName,
                 password: hashedPassword,
+                role: "manager",
                 id: realId
             })
             console.log("User Registered Successfully")
@@ -68,7 +71,7 @@ const registerUser = async (formData: FormData)=>{
         throw new Error(error)
     }
 
-    redirect('/login');
+    redirect('/auth/login');
 
 }
 
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,7 +1,53 @@
+import { registerUser } from "@/action/actions";
+import Link from "next/link";
+
 export default function RegisterPage() {
-    return (
-        <div>
-            Register
+  return (
+    <div className="bg-white flex flex-col gap-10 rounded-lg sm:w-1/2 md:w-[30%] border shadow-xl text-primary items-center justify-center p-4">
+      <h1 className="text-2xl font-bold text-center">
+        Welcome to Shift-Sync!{" "}
+      </h1>
+      <form action={registerUser} className=" flex flex-col gap-y-8">
+        <h2 className="text-xl font-bold text-center">Register</h2>
+        <div className="flex gap-4">
+          <input
+            type="text"
+            name="fname"
+            className="p-2 border-2 border-primary rounded-lg bg-inherit text-black"
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            name="lname"
+            className="p-2 border-2 border-primary rounded-lg bg-inherit text-black"
+            placeholder="Last Name"
+          />
         </div>
-    )
+        <input
+          type="text"
+          name="team"
+          className="p-2 border-2 border-primary rounded-lg bg-inherit text-black"
+          placeholder="Organization Name"
+        />
+        <input
+          type="text"
+          name="email"
+          className="p-2 border-2 border-primary rounded-lg bg-inherit text-black"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          className="p-2 border-2 border-primary rounded-lg bg-inherit text-black"
+          placeholder="Password"
+        />
+        <button type="submit" className="bg-primary p-3 text-white rounded-lg">
+          Register
+        </button>
+      </form>
+      <Link href="/auth/login" className=" underline">
+        Already have an account?
+      </Link>
+    </div>
+  );
 }

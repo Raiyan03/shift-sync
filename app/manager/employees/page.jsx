516a0b874410d90445eb5bcc0a2fb29809d6b436
Dhillon-Saab <ad70138@gmail.com>
2024-07-26 13:53:31 -0600

    still resolving

--- app/manager/employees/page.jsx
+++ app/manager/employees/page.jsx
@@ -1,32 +1,35 @@
 "use client";
 import { useState, useEffect } from "react";
 import { getUser } from "@/action/actions";
-import UserTable from "@/components/manager/employees/employees";
-import { getEmployeesDataFromDB } from "@/server/calls";
+import { getEmployeeData } from "@/lib/utilities";
 import Evaluation from "@/components/manager/evaluation/evaluation";
+import UserTable from "@/components/manager/employees/employees";
+
 export default function Employees() {
-  const [employeeData, setEmployeeData] = useState();
-  const fetchData = async () => {
-    const userData = await getUser();
-    const employees = await getEmployeesDataFromDB(userData.id);
-    const data = {
-      employees,
+    const [employeeData, setEmployeeData] = useState();
+    
+    const fetchData = async () => {
+      const userData = await getUser();
+      const employees = await getEmployeeData(userData.id);
+      console.log(employees);
+      const data = {
+        employees,
+      };
+  
+      setEmployeeData(data);
     };
-    setEmployeeData(data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <div className="flex flex-col space-y-4 p-4">
-      <UserTable
-        employeeData={employeeData}
-        setEmployeeData={setEmployeeData}
-      />
-      <div className="flex flex-col bg-secondary shadow-md border rounded-md p-4">
-        <h1 className="text-xl font-bold mb-4">Evaluation</h1>
-        <Evaluation />
-      </div>
-    </div>
-  );
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
+    
+    return (
+        <div className="flex flex-col space-y-4 p-4">
+            <UserTable employeeData={employeeData} setEmployeeData={setEmployeeData} />
+            <div className="flex flex-col bg-secondary shadow-md border rounded-md p-4">
+                <h1 className="text-xl font-bold mb-4">Evaluation</h1>
+                <Evaluation />
+            </div>
+        </div>
+    );
 }

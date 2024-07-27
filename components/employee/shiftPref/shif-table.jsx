9db16eafa5aad936248a3339caa906e74deeefe4
Ryan <ryansheikh003@gmail.com>
2024-07-27 15:53:15 -0600

    feat(ui): Style shift schedule
    Styled the employee schedule component
    
    Re organised time stamp and getDay fucntion into /lib/helper.js

--- components/employee/shiftPref/shif-table.jsx
+++ components/employee/shiftPref/shif-table.jsx
@@ -1,33 +1,26 @@
-const getDayName = (day) => {
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    return days[day];
-  };
-  
-  // Function to convert timestamp to time string
-const formatTime = (timestamp) => {
-    const date = new Date(timestamp);
-    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  };
-
+import { getDayName, formatTime} from "@/lib/employeeHelper"
 
 const ShiftTable = ({ schedule }) => {
+    console.log(schedule)
     return (
       <div className="bg-secondary p-2 border rounded-lg">
         <h1 className="text-xl">Schedule</h1>
-        <table>
+        <table className="w-full">
           <thead>
             <tr>
               <th className="p-2" >Day</th>
               <th className="p-2">Time</th>
               <th className="p-2">Shift Length (hours)</th>
+              <th>Request</th>
             </tr>
           </thead>
           <tbody>
             {schedule.map((item, index) => (
               <tr key={index}>
-                <td className="p-2">{getDayName(item.day)}</td>
-                <td className="p-2">{`${formatTime(item.shift.shift[0])} - ${formatTime(item.shift.shift[1])}`}</td>
+                <td className="p-2 text-center">{getDayName(item.day)}</td>
+                <td className="p-2 text-center">{`${formatTime(item.shift.shift[0])} - ${formatTime(item.shift.shift[1])}`}</td>
                 <td className="p-2 text-center">{item.shift.hours}hr</td>
+                <td className="p-2 text-center">{item.shift.requested ? "Requested" : "Not Requested"}</td>
               </tr>
             ))}
           </tbody>

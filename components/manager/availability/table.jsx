09fd5ae639aabe7fa05034eed2bc7fc7469cadb2
Ryan <ryansheikh003@gmail.com>
2024-07-31 16:38:08 -0600

    feat(ui) : Add loader on availability page
    Now while the employee prefernce data is being fetch a loader appears

--- components/manager/availability/table.jsx
+++ components/manager/availability/table.jsx
@@ -3,21 +3,27 @@ import { getUser } from "@/action/actions";
 import { useState, useEffect } from "react";
 import TableData from "@/components/manager/availability/table-data";
 import { getUserPreferencesForTheBackend } from "@/server/calls";
+import Loader from "@/components/loader";
 export default function Table() {
     const [schedule, setSchedule] = useState();
-
+    const [loading, setLoading] = useState(true);
     const fetchAndLogData = async () => {
       const userData = await getUser();
       const userId = userData.id
       const scheduleData = await getUserPreferencesForTheBackend(userId);
       scheduleData !== null? setSchedule(scheduleData) : ""
+      setLoading(false);
     };
   
     useEffect(() => {
       fetchAndLogData();
     }, []);
     return (
-        <div className="mt-4 flex flex-col bg-secondary border shadow-md rounded-lg p-4">
+    loading ? 
+    <div className=" flex items-center justify-center border shadow-md rounded-lg h-80">
+        <Loader />
+    </div> :
+        <div className="flex flex-col bg-secondary border shadow-md rounded-lg p-4">
         <h1 className='text-xl text-accent1 py-3'>
             Employee preferences
         </h1>
